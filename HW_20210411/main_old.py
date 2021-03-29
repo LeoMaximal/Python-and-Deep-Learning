e330ee6e7afd24328bb9972987f8f2ed9c67c8aa
LeoMaximal <LeoMaximal@outlook.com>
2021-03-29 17:23:21 +0800

    reconstructed code using WikiMedia API

---
+++ HW_20210411/main_old.py
@@ -0,0 +1,64 @@
+from utils_old import *
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog, QLabel, QPushButton, QGridLayout
+
+class wiki_rank(QDialog): #define a class to show the the weekly rank of articles from Wikiprdia
+    def __init__(self):
+        super().__init__()
+        
+        #set the elements of the window
+        self.setWindowTitle('Wikipedia: Weekly Top 10')
+        self.btnGet = QPushButton('Get Rank', self)
+        self.btnPre = QPushButton('Previous Week', self)
+        self.btnNext = QPushButton('Next Week', self)
+        self.btnQuit = QPushButton('Quit', self)
+        self.text = QLabel()
+        
+        #set the position of elements
+        layout = QGridLayout(self)
+        layout.addWidget(self.text, 0, 1, 3, 15)
+        layout.addWidget(self.btnGet, 4, 1, 1, 3)
+        layout.addWidget(self.btnPre, 4, 5, 1, 3)
+        layout.addWidget(self.btnNext, 4, 9, 1, 3)
+        layout.addWidget(self.btnQuit, 4, 13, 1, 3)
+
+        #connet button to function
+        self.btnGet.clicked.connect(self.show_rank)
+        self.btnPre.clicked.connect(self.show_pre)
+        self.btnNext.clicked.connect(self.show_next)
+        self.btnQuit.clicked.connect(self.close)
+
+    url = 'https://en.wikipedia.org/wiki/Wikipedia:Top_25_Report' #the url of the rank of this week
+
+    def show_rank(self):
+        global url
+        url = 'https://en.wikipedia.org/wiki/Wikipedia:Top_25_Report'
+        str=get_rank(url)
+        self.text.setText(str) #show  the rank
+
+    def show_pre(self):
+        global url #make sure that url can be editted
+        bs = BeautifulSoup(requests.get(url, proxies={
+        'https': 'http://127.0.0.1:1087', 'http': 'http://127.0.0.1:1087'}).content)
+        link = bs.find(name='span', attrs={'class': 'plainlinks'}) #get the url of the rank of the previous week
+        url = 'https://en.wikipedia.org/' + link.find('a')['href'] #make sure that the url is accessible
+        str=get_rank(url)
+        self.text.setText(str)
+
+    def show_next(self):
+        global url
+        if url == 'https://en.wikipedia.org/wiki/Wikipedia:Top_25_Report': #no 'next week' link on the page of this week
+            self.text.setText('NO DATA!')
+        else:
+            bs = BeautifulSoup(requests.get(url, proxies={
+            'https': 'http://127.0.0.1:1087', 'http': 'http://127.0.0.1:1087'}).content)
+            link = bs.find(name='span', attrs={'style':'float:right;'}) #get the url of the rank of the next week
+            url = 'https://en.wikipedia.org/' + link.find('a')['href']
+            str=get_rank(url)
+            self.text.setText(str)
+    
+if __name__ == '__main__':
+    a = QApplication(sys.argv)
+    ex = wiki_rank()
+    ex.show()
+    sys.exit(a.exec_())
--- HW_20210411/utils.py
+++ HW_20210411/utils.py
@@ -1,24 +1,60 @@
 import requests
-from bs4 import BeautifulSoup
+import json
 
-def get_rank(url): #this function can get the weekly rank of articles from Wikiprdia
-    bs = BeautifulSoup(requests.get(url, proxies={
-    'https': 'http://127.0.0.1:1087', 'http': 'http://127.0.0.1:1087'}).content) #use bs to get html
+def get_descript(title_link):
+    url = 'https://de.wikipedia.org/w/api.php?action=query&format=json&prop=description&titles=' + title_link
+    r = requests.get(url, proxies={'https': 'http://127.0.0.1:1087', 'http': 'http://127.0.0.1:1087'})
+    js = json.loads(r.text)
+    info = js['query']['pages']
+    for item in info:
+        info = info[item]
+    if info.__contains__('description'):
+        return info['description']
+    else:
+        return None
 
-    title = str((bs.find(class_="mw-headline")).contents) #get title of the report page
-    str_rank = title[title.index("M"):title.index(")")+1]+'\n' #store title in str_rank
-    
-    table = bs.find(class_='wikitable') #get the main body of the rank table
-    flag = 0 #if this column represents the rank, the next column represents article titles which will be used
-    count = 1 #number of articles
-    for item in table.find_all('td'): #find all units of the table
-        if flag == 1: #if the previous column represents the rank
-            str_rank=str_rank+'\n'+str(count-1)+'  '+str(item.find('a')['title']) #store title of articles
-            #print('%2d  %s' % (count - 1, item.find('a')['title']))
-            flag = 0
-        if str(item) == '<td>' + str(count) + '\n</td>': #if the content represents the rank
-            flag = 1
-            count = count + 1
-        if count == 12:
+def get_rank():
+    url = 'https://de.wikipedia.org/w/api.php?action=query&format=json&list=mostviewed&pvimlimit=15'
+    r = requests.get(url, proxies={'https': 'http://127.0.0.1:1087', 'http': 'http://127.0.0.1:1087'})
+    js = json.loads(r.text)
+    data = js['query']['mostviewed']
+    count = 0
+    str_rank='<p><b style="font-size:18px;"> Daily Top 10 </b><br><br>'
+    for item in data:
+        if count > 9:
             break
+        if item['ns'] == 0:
+            count = count + 1
+            title = item['title']
+            viewed = str(item['count'] / 1000)
+            title_link = title.replace(' ', '_')
+            str_rank = str_rank + '<b style="color:black;font-size:16px;">' + str(count) + \
+            '. <a href=https://de.wikipedia.org/wiki/' + title_link + '>' + title + \
+            '</a>  viewed:  '+viewed[0:viewed.index('.') + 2]+'K</b><br>'
+
+            description = get_descript(title_link)
+            if description:
+                str_rank = str_rank + description + '<br>'
+    str_rank = str_rank + '<\p>'
     return str_rank
+
+def get_rand():
+    url = 'https://de.wikipedia.org/w/api.php?action=query&format=json&list=random&rnlimit=50'
+    r = requests.get(url, proxies={'https': 'http://127.0.0.1:1087', 'http': 'http://127.0.0.1:1087'})
+    js = json.loads(r.text)
+    str_rand = '<p><b style="font-size:18px;"> Random Pages </b><br><br>'
+    count = 0
+    for item in js['query']['random']:
+        if count > 4:
+            break
+        if item['ns'] == 0:
+            count = count + 1
+            title = item['title']
+            title_link = title.replace(' ', '_')
+            str_rand = str_rand + '<b><a style="color:black;font-size:16px;" href=https://de.wikipedia.org/wiki/' \
+            + title_link + '>' + title + '</a></b><br>'
+            description = get_descript(title_link)
+            if description:
+                str_rand = str_rand + description + '<br>'
+    str_rand = str_rand + '</p>'
+    return str_rand
